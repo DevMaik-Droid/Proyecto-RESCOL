bda366d03c8c61c682a84f6a50917fc1df05c6ec
DevMaik <miguel.040.net@gmail.com>
2024-12-27 16:04:25 -0400

    Librerias Postgres

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 const aulaRoutes = require("./api/routes/aula-routes");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
 
 app.use("/api/aulas", aulaRoutes);
 
